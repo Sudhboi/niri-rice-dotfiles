bd275a0f58445e14e49107ccca3407a566ab020f
Sudhir <sudhboiii@gmail.com>
2025-10-28 18:48:46 -0300

    Added Push Script to dotfiles

---
+++ push.sh
@@ -0,0 +1,21 @@
+#!/bin/zsh
+
+echo "Enter Commit Message: "
+read commit_message
+
+export DOTFILES="/home/sudhirk/.backups/dotfiles"
+
+
+cp -r ~/.config/niri $DOTFILES/.config
+cp -r ~/.config/btop $DOTFILES/.config
+cp -r ~/.config/cava $DOTFILES/.config
+cp -r ~/.config/fastfetch $DOTFILES/.config
+
+
+cd $DOTFILES
+
+git add -A
+
+git commit -m $commit_message
+git push -u origin main
+
